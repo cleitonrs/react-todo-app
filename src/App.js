78c6611625a9d683d26aff615a7a4fc6192c36c9
cleitonrs <rscleitonrs@gmail.com>
2021-11-23 11:34:27 -0300

    todo form component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import TodoForm from './components/TodoForm';
 
 function App() {
   return (
     <div className="todo-app">
-      <h1>Todo app</h1>
+      <TodoForm />
     </div>
   );
 }
---
+++ src/components/TodoForm.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+const TodoForm = (props) => {
+  const [input, setInput] = useState("");
+
+  const handleChange = (event) => {
+    setInput(event.target.value)
+  }
+
+  // Prevent refresh page on submit
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    // props.onSubmit({
+    //   id: Math.floor(Math.random() * 10000), // Generate a randomic id number of 0 - 10000
+    //   text: input
+    // })
+
+    setInput('')
+  }
+
+  return (
+    <form className="todo__form" onSubmit={handleSubmit}>
+      <input
+        className="todo__input"
+        type="text"
+        placeholder="Add a todo"
+        value={input}
+        name="text"
+        onChange={handleChange}
+      />
+      <button className="todo__button">Add todo</button>
+    </form>
+  );
+};
+
+export default TodoForm;
